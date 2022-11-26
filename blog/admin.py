b69870b40775eeb81cc778a62a24ec58554b7b75
Charlie <charlottevfarmer@gmail.com>
2022-11-26 09:47:30 +0000

    Update admin.py file

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+# Decorator to register Post model & PostAdmin class with Admin Site
+# Post Admin - Allows creation & management of posts
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+
+# Decorator to register Comment model & CommentAdmin class with Admin Site
+# Comment Admin - Allows management of comments
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    # Comments must be approved before they are posted
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+# Categories for each blog post
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='default')
+    excerpt = models.TextField(blank=True)
+    category = models.CharField(max_length=200, default='general')
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+# Auto-generated admin categories for comments
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=90)
+    body = models.TextField()
+    email = models.EmailField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
