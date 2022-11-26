44cf0c96c765fc7fd4bc331522e00184d659d177
Charlie <charlottevfarmer@gmail.com>
2022-11-26 10:32:25 +0000

    Add post method to PostDetail view for commenting

--- blog/views.py
+++ blog/views.py
@@ -33,3 +33,36 @@ class PostDetail(View):
                 "comment_form": CommentForm(),
             },
         )
+
+    # class method to allow users to comment
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            # returns an empty comment form
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm(),
+            },
+        )
