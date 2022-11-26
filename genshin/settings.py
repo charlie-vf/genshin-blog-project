c0e476fc5e9e790e14c03e1778468026384c1a30
Charlie <charlottevfarmer@gmail.com>
2022-11-26 09:36:42 +0000

    Install remaining packages

--- genshin/settings.py
+++ genshin/settings.py
@@ -32,6 +32,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['genshin-blog-project.herokuapp.com', 'localhost']
 
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,11 +42,24 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
+    'crispy_forms',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
